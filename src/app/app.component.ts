87facfeb1cc298928c43a8abbc91334d7bda4b4c
Baptiste LYET <baptiste.lyet@agixis.com>
2024-01-23 14:43:00 +0100

    test responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'DrumsPatternLibrary';
+export class AppComponent implements OnInit {
+  isMobileDisplay: boolean = true;
+  constructor(private responsive: BreakpointObserver) {
+  }
+
+  ngOnInit(): void {
+    this.responsive.observe([
+      Breakpoints.Web,
+    ])
+      .subscribe(result => {
+        this.isMobileDisplay = !result.matches;
+      });
+  }
 }
