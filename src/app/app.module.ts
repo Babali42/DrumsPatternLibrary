bc49d169ed35e06b0d0f883dff2106aad36d6713
babali42 <bab07ali@gmail.com>
2024-04-25 22:35:23 +0200

    suppression du component track

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {SequencerComponent} from './components/sequencer/sequencer.component';
 import {HttpClientModule} from '@angular/common/http';
-import {TrackComponent} from './components/sequencer/track/track.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SequencerComponent,
-    TrackComponent,
+    SequencerComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/sequencer/sequencer.component.ts
+++ src/app/components/sequencer/sequencer.component.ts
@@ -1,45 +1,16 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {DataService} from '../../services/data.service';
-import {Convert, JsonBeat} from '../../models/primary/jsonBeat';
+import {Component, Input} from '@angular/core';
 import {SoundService} from '../../services/sound.service';
 import {Beat} from '../../models/beat';
-import {BehaviorSubject} from 'rxjs';
 
 @Component({
   selector: 'sequencer',
   templateUrl: './sequencer.component.html',
   styleUrls: ['./sequencer.component.scss']
 })
-export class SequencerComponent implements OnInit {
+export class SequencerComponent {
+  @Input() beat: Beat = new Beat('', 120, []);
 
-  @Input() fileNameBehaviourSubject: BehaviorSubject<string>
-  beat: Beat = new Beat('', 120, []);
-
-  constructor(private dataService: DataService, public soundService: SoundService) {
-    this.fileNameBehaviourSubject = new BehaviorSubject<string>("metal");
-  }
-
-  ngOnInit(): void {
-    this.fileNameBehaviourSubject.subscribe(fileName => {
-      this.dataService.getData<JsonBeat>(fileName, 'beats/').subscribe((result: JsonBeat) => {
-        this.beat = Convert.toBeat(result);
-        if (this.soundService.isPlaying)
-          this.soundService.pause();
-        this.soundService.reset();
-        this.soundService.setBpm(this.beat.bpm);
-        this.soundService.setTracks(this.beat.tracks);
-        this.soundService.setStepNumber(this.beat.tracks[0].steps.length);
-      });
-    });
-  }
-
-  toggleIsPlaying(): void {
-    this.soundService.playPause().then(
-      () => {
-      },
-      () => {
-      }
-    );
+  constructor(public soundService: SoundService) {
   }
 }
 
