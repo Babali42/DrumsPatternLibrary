7b74da779275db627b48214d518f316b467c9617
Baptiste <bab07ali@gmail.com>
2024-06-28 23:34:36 +0200

    refacto - création d'un soundGeneratorService

---
+++ src/app/services/sound-generator.service.ts
@@ -0,0 +1,44 @@
+import {Injectable} from "@angular/core";
+import {Track} from "../models/track";
+import {Sample} from "../models/sample";
+
+@Injectable({providedIn: "root"})
+export class SoundGeneratorService {
+  //Rendered sound (twice the grid length)  ████░░░░
+  //Played sound (the doted part is looped) ████░░░░░░░░░░░░░░░░░░░░░░░░
+  //Used to avoid sound clip ;)
+  async getRenderedBuffer(tracks: Track[], samples: Sample[], bpm: number, stepNumber: number): Promise<AudioBuffer> {
+    const tickLength = this.getTickLength(bpm);
+    const offlineContext: OfflineAudioContext = new OfflineAudioContext(1, stepNumber * 2 * tickLength * 44100, 44100);
+    tracks.forEach((track: Track) => {
+      const trackSteps = this.getDuplicatedTrackSteps(track);
+      trackSteps.forEach((beat: boolean, i: number) => {
+        if (!beat)
+          return;
+
+        const audioBuffer = samples.find(x => x.fileName === track.fileName)!.sample!;
+        const audioBufferSourceNode = offlineContext.createBufferSource();
+        audioBufferSourceNode.buffer = audioBuffer;
+        audioBufferSourceNode.connect(offlineContext.destination);
+
+        const when = i * tickLength;
+        audioBufferSourceNode.start(when);
+      });
+    });
+
+    offlineContext.oncomplete = (event: OfflineAudioCompletionEvent) => {
+      return event.renderedBuffer;
+    }
+    return await offlineContext.startRendering();
+  }
+
+  private getDuplicatedTrackSteps(track: Track): boolean[] {
+    const duplicatedTracks = [...track.steps]
+    duplicatedTracks.push(...track.steps);
+    return duplicatedTracks;
+  }
+
+  private getTickLength(bpm: number): number {
+    return 60 / bpm / 4;
+  };
+}
