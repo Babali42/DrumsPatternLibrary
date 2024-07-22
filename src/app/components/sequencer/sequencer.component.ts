f9a76d56aacf6bcc6372293ed6d1938164b8f852
babali42 <bab07ali@gmail.com>
2024-07-23 00:17:35 +0200

    refacto : add all or nothing json parse error handling

--- src/app/components/sequencer/sequencer.component.ts
+++ src/app/components/sequencer/sequencer.component.ts
@@ -2,6 +2,7 @@ import {Component, Input} from '@angular/core';
 import {SoundService} from '../../services/sound.service';
 import {Beat} from '../../models/beat';
 import { NgFor } from '@angular/common';
+import {StepLengths} from "../../models/primary/jsonBeat";
 
 @Component({
     selector: 'sequencer',
@@ -15,5 +16,7 @@ export class SequencerComponent {
 
   constructor(public soundService: SoundService) {
   }
+
+  protected readonly StepLengths = StepLengths;
 }
 
