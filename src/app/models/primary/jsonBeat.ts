551c2f6dad8b52f010e2b4671a296d09b30874e0
babali42 <bab07ali@gmail.com>
2024-07-23 00:17:35 +0200

    refacto : add all or nothing json parse error handling

--- src/app/models/primary/jsonBeat.ts
+++ src/app/models/primary/jsonBeat.ts
@@ -8,13 +8,21 @@ export interface JsonBeat {
   tracks: JsonTrack[];
 }
 
-export class Convert{
-  public static toBeat(jsonBeat: JsonBeat): Beat{
+export const StepLengths = {
+  sixteen: 16,
+  thirty_two: 32,
+  sixty_four: 64
+}
+
+export class Convert {
+  public static toBeat(jsonBeat: JsonBeat): Beat {
     const tracks = jsonBeat.tracks.map(x => Convert.toTrack(x));
     return new Beat(jsonBeat.name, jsonBeat.bpm, tracks);
   }
 
   private static toTrack(jsonTrack: JsonTrack): Track {
+    if (jsonTrack.steps.length != StepLengths.sixteen && jsonTrack.steps.length != StepLengths.thirty_two && jsonTrack.steps.length != StepLengths.sixty_four)
+      throw new Error(`Error during casting json track steps, please count steps of ${jsonTrack.name} track`);
     return new Track(jsonTrack.name, jsonTrack.fileName, Array.from(jsonTrack.steps).map(x => x == "X"));
   }
 }
