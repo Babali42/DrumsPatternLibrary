127b974326e941dd180bdc7e370dd346db6a723a
babali42 <bab07ali@gmail.com>
2024-08-19 22:34:10 +0200

    ajout de jest

---
+++ src/app/models/primary/jsonBeat.test.ts
@@ -0,0 +1,13 @@
+import {describe, expect} from '@jest/globals';
+import {Convert} from "./jsonBeat";
+
+describe('Primary beat', () => {
+  it.each([
+    ["XXXXXXXXXXXXXXXX", 16],
+    ["XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX", 32],
+    ["XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX", 64]
+  ])('with step %p should be a %p length grid', (steps : string, result: number) => {
+    const beat = Convert.toBeat({name: "", bpm: 128, tracks: [{name: "", fileName: "", steps: steps}]});
+    expect(beat.tracks[0].steps.length).toEqual(result);
+  });
+});
