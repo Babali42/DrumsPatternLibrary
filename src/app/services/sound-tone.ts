cfe5e81466e594a335fb1ea8562c4da2ba598f19
Baptiste <bab07ali@gmail.com>
2024-08-22 10:58:41 +0200

    tone : create the sequence with the track

--- src/app/services/sound-tone.ts
+++ src/app/services/sound-tone.ts
@@ -2,6 +2,7 @@ import {Injectable} from "@angular/core";
 import {Track} from "../models/track";
 import {Sound} from "./sound";
 import * as Tone from 'tone';
+import {Sequence} from "tone";
 
 @Injectable({
   providedIn: 'root'
@@ -10,50 +11,20 @@ export class SoundTone implements Sound {
   bpm: number = 120;
   isPlaying: boolean = false;
   index: number = 0;
-  private kick: Tone.Player;
-  private snare: Tone.Player;
-  private hihat: Tone.Player;
-  private crash: Tone.Player;
-  private seq: Tone.Sequence<string | null>;
+  private sequence: Sequence<any> = new Sequence();
   private stepNumber: number = 16;
-  private _tracks: Track[] = [];
-
-  constructor() {
-
-    // Initialize the players with audio files
-    this.kick = new Tone.Player('assets\\sounds\\techno\\kick.wav').toDestination();
-    this.snare = new Tone.Player('assets\\sounds\\techno\\snare.wav').toDestination();
-    this.hihat = new Tone.Player('assets\\sounds\\techno\\hat.wav').toDestination();
-    this.crash = new Tone.Player('assets\\sounds\\metal\\crash.wav').toDestination();
-
-    // Initialize the sequence
-    this.seq = new Tone.Sequence(
-      (time, note) => {
-        if (note === 'kick') {
-          this.kick.start(time);
-        } else if (note === 'snare') {
-          this.snare.start(time);
-        } else if (note === 'hihat') {
-          this.hihat.start(time);
-        } else if (note === 'crash') {
-          this.crash.start(time);
-        }
-      },
-      ['kick', 'hihat', 'snare', 'hihat', 'snare', 'hihat', 'snare', 'hihat', 'kick', 'hihat', 'snare', 'hihat', 'kick', null, 'snare', 'snare' ],
-      '16n'
-    );
-  }
 
   pause(): void {
   }
 
   playPause(): Promise<void> {
-    this.seq.start(0);
+    this.sequence.start(0);
     Tone.Transport.start();
     return Promise.resolve(undefined);
   }
 
   reset(): void {
+    this.sequence = new Sequence();
   }
 
   setBpm(bpm: number): void {
@@ -65,6 +36,30 @@ export class SoundTone implements Sound {
   }
 
   setTracks(tracks: Track[]): void {
-    this._tracks = tracks;
+    Tone.start().then(() => {
+      const players = {};
+
+      tracks.forEach(track => {
+        // @ts-ignore
+        players[track.name] = new Tone.Player({
+          url: "assets\\sounds\\" + track.fileName,
+          loop: false, // Ensure the sample plays fully without looping
+        }).toDestination();
+      });
+
+      // Create a single sequence to orchestrate all tracks
+      const stepsPerTrack = tracks.map(track => track.steps);
+      const totalSteps = tracks[0].steps.length;
+
+      this.sequence = new Tone.Sequence((time, stepIndex) => {
+        tracks.forEach((track, trackIndex) => {
+          const shouldPlay = stepsPerTrack[trackIndex][stepIndex];
+          if (shouldPlay) {
+            // @ts-ignore
+            players[track.name].start(time);
+          }
+        });
+      }, Array.from({length: totalSteps}, (_, i) => i), "16n");
+    });
   }
 }
