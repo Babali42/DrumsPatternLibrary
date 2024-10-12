8040a4b69424726816cf0d943c51a3e7277779cb
Baptiste <bab07ali@gmail.com>
2024-10-12 12:55:37 +0200

    add jasmine karma test

---
+++ src/app/adapters/test.spec.ts
@@ -0,0 +1,20 @@
+import {JsonFilesService} from "../services/json-files.service";
+import {TestBed} from "@angular/core/testing";
+import {HttpClient} from "@angular/common/http";
+
+describe('HeroAdapterService', () => {
+  let service: JsonFilesService;
+  let httpClientSpy: jasmine.SpyObj<HttpClient>;
+
+  beforeEach(() => {
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get', 'post', 'put', 'delete']);
+    TestBed.configureTestingModule({
+      providers: [{ provide: HttpClient, useValue: httpClientSpy }]
+    });
+    service = TestBed.inject(JsonFilesService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
