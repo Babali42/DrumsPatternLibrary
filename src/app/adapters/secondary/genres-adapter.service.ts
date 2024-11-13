ee2486f7cbd1f2ce5b2b578ef377bca4df5a578b
Baptiste <bab07ali@gmail.com>
2024-11-13 17:58:43 +0100

    refacto genre adapter to use effect in order to have a simpler error managment

--- src/app/adapters/secondary/genres-adapter.service.ts
+++ src/app/adapters/secondary/genres-adapter.service.ts
@@ -1,26 +1,24 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
-import { catchError, Observable } from 'rxjs';
 import IManageGenres from "../../domain/ports/secondary/i-manage-genres";
 import {Genre} from "../../domain/genre";
+import {Effect} from 'effect';
+import {lastValueFrom} from "rxjs";
 
-@Injectable({ providedIn: 'root' })
+@Injectable({providedIn: 'root'})
 export class GenresAdapterService implements IManageGenres {
 
   private genresUrl = 'api/genres';
 
   constructor(private http: HttpClient) { }
 
-  getGenres(): Observable<Genre[]> {
-    return this.http.get<Genre[]>(this.genresUrl).pipe(
-      catchError(this.handleHttpError())
-    );
-  }
+  getGenres(): Promise<Genre[]> {
+    const effect = Effect.tryPromise({
+      try: () => lastValueFrom(this.http.get<Genre[]>(this.genresUrl)),
+      catch: () => new Error("Can't get genres")
+    });
 
-   private handleHttpError() {
-    return (error: any): Observable<any> => {
-      throw new Error(error.body.error);
-    };
+    return Effect.runPromise(effect);
   }
 }
