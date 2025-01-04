a57d9cf4efd472fc12d8765bd31b0ce6a8bfdb56
Baptiste <bab07ali@gmail.com>
2025-01-04 20:48:32 +0100

    add test on sound service

--- frontend/src/app/services/sound/sound.service.spec.ts
+++ frontend/src/app/services/sound/sound.service.spec.ts
@@ -4,22 +4,40 @@ import { TestBed } from "@angular/core/testing";
 import {LoadingBarService} from "@ngx-loading-bar/core";
 
 describe("sound", () => {
-  let mockSoundGeneratorService: Partial<SoundGeneratorService>;
   let soundService: SoundService;
+  let soundGeneratorService: SoundGeneratorService;
 
   beforeEach(() => {
+    const mockSoundGeneratorService = {
+      getRenderedBuffer: jasmine.createSpy('getRenderedBuffer').and.returnValue(Promise.resolve(new AudioBuffer( {
+        length: 10,
+        numberOfChannels: 2,
+        sampleRate: 44000
+      })))
+    };
+
     TestBed.configureTestingModule({
       providers: [
         SoundService,
-        { provide: SoundGeneratorService, useValue: mockSoundGeneratorService },
+        {
+          provide: SoundGeneratorService,
+          useValue: mockSoundGeneratorService
+        },
         LoadingBarService
       ]
     });
 
     soundService = TestBed.inject(SoundService);
+    soundGeneratorService = TestBed.inject(SoundGeneratorService);
   });
 
   it("should create the sound service", () => {
     expect(soundService).toBeTruthy();
+  });
+
+  it("should call the soundGenerator once when play twice", async () => {
+    await soundService.playPause();
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    expect(soundGeneratorService.getRenderedBuffer).toHaveBeenCalledTimes(1);
   });
 })
