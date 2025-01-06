26d64baec383cc1692e1894cc3a0b2d1f7c3d787
Baptiste <bab07ali@gmail.com>
2025-01-06 21:04:17 +0100

    add style to the bpm input component and adjust sequencer

--- frontend/src/app/components/bpm-input/bpm-input.component.ts
+++ frontend/src/app/components/bpm-input/bpm-input.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-bpm-input',
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './bpm-input.component.scss'
 })
 export class BpmInputComponent {
-
+  @Input() bpm : number = 128;
 }
