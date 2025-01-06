d8b67c0de55202fc27c6172121859b038a63aaf2
Baptiste <bab07ali@gmail.com>
2025-01-06 21:53:15 +0100

    modify input value with input or buttons

--- frontend/src/app/components/bpm-input/bpm-input.component.ts
+++ frontend/src/app/components/bpm-input/bpm-input.component.ts
@@ -8,5 +8,23 @@ import {Component, Input} from '@angular/core';
   styleUrl: './bpm-input.component.scss'
 })
 export class BpmInputComponent {
+  maxBpm = 300;
+  minBpm = 20;
   @Input() bpm : number = 128;
+
+  incrementBpm() {
+    this.bpm = Math.min(this.bpm+1, this.maxBpm);
+  }
+
+  decrementBpm() {
+    this.bpm = Math.max(this.bpm-1, this.minBpm);
+  }
+
+  updateNumber(event: Event): void {
+    const inputElement = event.target as HTMLInputElement;
+    let newValue = Number(inputElement.value);
+    newValue = Math.min(newValue, this.maxBpm);
+    newValue = Math.max(newValue, this.minBpm);
+    this.bpm = newValue;
+  }
 }
